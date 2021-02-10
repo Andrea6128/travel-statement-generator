e2503192aaa71f2ed721dbbba6056fabee66387a
Andrea Petriku <iam.andreapetriku@gmail.com>
2021-02-10 14:29:45 +0100

    added petrol consumption calculations

--- generate.py
+++ generate.py
@@ -13,6 +13,15 @@ from openpyxl.styles import Alignment
 import random
 import datetime
 import sys
+import decimal
+
+
+def getPetrol():
+    petrolPrice = random.randint(120, 135) / 100
+    consumption = 22 / 100  # 22 litres per 100 km
+    consumptionPrice = consumption * petrolPrice
+
+    return [petrolPrice, consumptionPrice]
 
 
 def randomTime(routeLength):
@@ -37,15 +46,19 @@ def randomTime(routeLength):
     eveningAdd = datetime.timedelta(hours=routeLengthHour, minutes=routeLengthMinute + salt)
     eveningEndTime = eveningStartTime + eveningAdd
 
+    # count total time
+    totalTime = str(eveningEndTime - morningStartTime)[:-3]
+    
+    # convert times to strings and cut unwanted chars
     morningStartTime = str(morningStartTime)[10:-3]
     morningEndTime = str(morningEndTime)[10:-3]
     eveningStartTime = str(eveningStartTime)[10:-3]
     eveningEndTime = str(eveningEndTime)[10:-3]
 
-    return [morningStartTime, morningEndTime, eveningStartTime, eveningEndTime]
+    return [morningStartTime, morningEndTime, eveningStartTime, eveningEndTime, totalTime]
 
 
-def dayRoute(routeType):
+def dayRoute():
     """ generate random start city """
 
     dayRouteResult = []
@@ -53,18 +66,17 @@ def dayRoute(routeType):
     randomStartCity = random.randint(1, 71)  # select random city combination from the base city
     randomStartCity2 = random.randint(72, 2556)  # select random city combination from the other cities except first
 
-    if routeType == 4:
-        cityFrom = ws['A'+str(randomStartCity)].value
-        cityTo = ws['B'+str(randomStartCity)].value
-        distance = ws['C'+str(randomStartCity)].value
-        travelTime = ws['D'+str(randomStartCity)].value
+    cityFrom = ws['A'+str(randomStartCity)].value
+    cityTo = ws['B'+str(randomStartCity)].value
+    distance = ws['C'+str(randomStartCity)].value
+    travelTime = ws['D'+str(randomStartCity)].value
 
-        dayRouteResult.append(cityFrom)
-        dayRouteResult.append(cityTo)
-        dayRouteResult.append(str(distance))
-        dayRouteResult.append(str(travelTime))
+    dayRouteResult.append(cityFrom)
+    dayRouteResult.append(cityTo)
+    dayRouteResult.append(str(distance))
+    dayRouteResult.append(str(travelTime))
 
-        return dayRouteResult
+    return dayRouteResult
     
 
 def fillSheet(startRow, startColumn, startDate, numberOfDays):
@@ -72,52 +84,54 @@ def fillSheet(startRow, startColumn, startDate, numberOfDays):
 
     for day in range(numberOfDays):
 
-        # TODO: remove this, singleDay is not needed anymore
-
-        # singleDay generates how many routes per day will be done (1, 2 or 3)
-        # singleDay = random.randrange(4, 8, 2)  # (start, stop, step)
-
-        singleDay = 4  # only 4 lines per route to save space on sheet
-
-        getStartRoute = dayRoute(singleDay)  # get dayRoute function result
+        getStartRoute = dayRoute()  # get dayRoute function result
         getRandomTime = randomTime(int(getStartRoute[2]))  # get start hour, end hour and division
 
-        if singleDay == 4:  # if route between 2 cities
-            for event in range(singleDay):
+        for event in range(4):
 
-                # way there ->
-                if event == 0:
-                    startDateStr = startDate.strftime('%d.%m.%Y')  # starting date (usualy 1.1.2020)
-                    ws.cell(row=startRow, column=startColumn, value=startDateStr)  # fill 1st cell with date
+            # way there ->
+            if event == 0:
+                startDateStr = startDate.strftime('%d.%m.%Y')  # starting date (usualy 1.1.2020)
+                ws.cell(row=startRow, column=startColumn, value=startDateStr)  # fill 1st cell with date
 
-                    ws.cell(row=startRow, column=startColumn+1, value="odchod")  # fill odchod
-                    ws.cell(row=startRow, column=startColumn+2, value=getStartRoute[0])  # fill start city
-                    ws.cell(row=startRow, column=startColumn+3, value=getRandomTime[0]).alignment = Alignment(vertical="center", horizontal="center")  # morning start time
-                    ws.cell(row=startRow, column=startColumn+4, value="AUS").alignment = Alignment(vertical="center", horizontal="center")
-                    ws.cell(row=startRow, column=startColumn+5, value=getStartRoute[2]).alignment = Alignment(vertical="center", horizontal="center")  # fill km
-                    # ws.cell(row=startRow, column=startColumn+6, value=getRandomWorkTime[0]).alignment = Alignment(vertical="center", horizontal="center")  # fill start work time
-                if event == 1:
-                    ws.cell(row=startRow, column=startColumn+1, value="príchod")  # fill prichod
-                    ws.cell(row=startRow, column=startColumn+2, value=getStartRoute[1])  # fill destination city
-                    ws.cell(row=startRow, column=startColumn+3, value=getRandomTime[1]).alignment = Alignment(vertical="center", horizontal="center")  # morning end time
-                    # ws.cell(row=startRow, column=startColumn+6, value=getRandomWorkTime[1]).alignment = Alignment(vertical="center", horizontal="center")  # fill end work time
+                ws.cell(row=startRow, column=startColumn+1, value="odchod")  # fill odchod
+                ws.cell(row=startRow, column=startColumn+2, value=getStartRoute[0])  # fill start city
+                ws.cell(row=startRow, column=startColumn+3, value=getRandomTime[0]).alignment = Alignment(vertical="center", horizontal="center")  # morning start time
+                ws.cell(row=startRow, column=startColumn+4, value="AUS").alignment = Alignment(vertical="center", horizontal="center")
+                ws.cell(row=startRow, column=startColumn+5, value=getStartRoute[2]).alignment = Alignment(vertical="center", horizontal="center")  # km
+                petrolPrice = getPetrol()  # get random petrol price
+                petrolPrice = float(round(decimal.Decimal(petrolPrice[1] * float(getStartRoute[2])), 2))
+                ws.cell(row=startRow, column=startColumn+7, value=petrolPrice).alignment = Alignment(vertical="center", horizontal="center")  # petrol price
+            if event == 1:
+                ws.cell(row=startRow, column=startColumn+1, value="príchod")  # fill prichod
+                ws.cell(row=startRow, column=startColumn+2, value=getStartRoute[1])  # fill destination city
+                ws.cell(row=startRow, column=startColumn+3, value=getRandomTime[1]).alignment = Alignment(vertical="center", horizontal="center")  # morning end time
 
-                # way back <-
-                if event == 2:
-                    ws.cell(row=startRow, column=startColumn+1, value="odchod")  # fill odchod
-                    ws.cell(row=startRow, column=startColumn+2, value=getStartRoute[1])  # fill destination city
-                    ws.cell(row=startRow, column=startColumn+3, value=getRandomTime[2]).alignment = Alignment(vertical="center", horizontal="center")  # evening start time
-                    ws.cell(row=startRow, column=startColumn+4, value="AUS").alignment = Alignment(vertical="center", horizontal="center")
-                    ws.cell(row=startRow, column=startColumn+5, value=getStartRoute[2]).alignment = Alignment(vertical="center", horizontal="center")  # fill km
-                if event == 3:
-                    ws.cell(row=startRow, column=startColumn+1, value="príchod")  # fill prichod
-                    ws.cell(row=startRow, column=startColumn+2, value=getStartRoute[0])  # fill start city
-                    ws.cell(row=startRow, column=startColumn+3, value=getRandomTime[3]).alignment = Alignment(vertical="center", horizontal="center")  # evening end time
+            # way back <-
+            if event == 2:
+                ws.cell(row=startRow, column=startColumn+1, value="odchod")  # fill odchod
+                ws.cell(row=startRow, column=startColumn+2, value=getStartRoute[1])  # fill destination city
+                ws.cell(row=startRow, column=startColumn+3, value=getRandomTime[2]).alignment = Alignment(vertical="center", horizontal="center")  # evening start time
+                ws.cell(row=startRow, column=startColumn+4, value="AUS").alignment = Alignment(vertical="center", horizontal="center")
+                ws.cell(row=startRow, column=startColumn+5, value=getStartRoute[2]).alignment = Alignment(vertical="center", horizontal="center")  # km
+                ws.cell(row=startRow, column=startColumn+7, value=petrolPrice).alignment = Alignment(vertical="center", horizontal="center")  # petrol price
+            if event == 3:
+                ws.cell(row=startRow, column=startColumn+1, value="príchod")  # fill prichod
+                ws.cell(row=startRow, column=startColumn+2, value=getStartRoute[0])  # fill start city
+                ws.cell(row=startRow, column=startColumn+3, value=getRandomTime[3]).alignment = Alignment(vertical="center", horizontal="center")  # evening end time
 
-                startRow += 1
+            startRow += 1
 
-            # increment day by 1
-            startDate = startDate + datetime.timedelta(days=1)
+        
+        # increment day by 1
+        startDate = startDate + datetime.timedelta(days=1)
+
+    # footer - write petrol column sum
+    petrolValueList = []
+    for row in range(5, 128, 2):
+        petrolValueList.append(ws.cell(row=row, column=8).value)
+    sumOfValueList = sum(petrolValueList)
+    ws.cell(row=129, column=8, value=sumOfValueList)
 
 
 # main
@@ -128,6 +142,6 @@ if __name__ == '__main__':
     ws = wb2.active  # set 2nd excel active
 
     # generateDates params: startRow, startColumn, startDate, numberOfDays
-    fillSheet(5, 1, datetime.datetime.strptime('2020-02-01', '%Y-%m-%d'), 31)
+    fillSheet(5, 1, datetime.datetime.strptime('2020-01-01', '%Y-%m-%d'), 31)
     wb2.save("output.xlsx")
     print("Done!")
